495e55b8cd8a40300899322bdea87fb8d09a6ceb
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-05-19 17:26:55 -0400

    update

--- linked_list.rb
+++ linked_list.rb
@@ -14,39 +14,114 @@ class LinkedList
   end
 
   def append(value)
-    
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next = new_node
+      @tail = new_node
+    end
   end
 
   def prepend(value)
-
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      new_node.next = @head
+      @head = new_node
+    end
   end
 
-  def size
-
+  def traverse
+    current_node = @head
+    until current_node.nil?
+      puts current_node.data
+      current_node = current_node.next
+    end
   end
 
-  def head #returns the total number of nodes in the list
 
+  def size #returns the total number of nodes in the list
+    current_node = @head
+    count = []
+    until current_node.nil?
+      count << current_node.data
+      current_node = current_node.next
+    end
+    puts "The size of this list is #{count.size}."
   end
 
-  def tail #returns the first node in the list
-
+  def head #returns the first node in the list
+    puts "Head: #{@head}"
   end
 
-  def at(index) #returns the last node in the list
-
+  def tail #returns the last node in the list
+    puts "Tail: #{@tail}"
   end
 
-  def pop #returns the node at the given index
-
+  def at(index) #returns the node at the given index
+    current_node = @head
+    count = 1
+    until count == index
+      if count.nil? || index.nil?
+        break
+      elsif count == index
+        puts "#{current_node} is at index #{index}."
+      else
+        current_node = current_node.next
+      end
+    end
   end
 
-  def contains?(value) #returns true if the passed in value is in the list and otherwise returns false.
+  def pop 
+    return nil if @head.nil?
 
+    if @head == @tail
+      popped_node = @head
+      @head = nil
+      @tail = nil
+    else
+      current_node = @head
+      until current_node == @tail
+        current_node = current_node.next
+      end
+      popped_node = @tail
+      current_node.next = nil
+      @tail = current_node
+    end
+
+    popped_node
+  end
+
+  def contains?(value)  #returns true if the passed in value is in the list and otherwise returns false.
+    current_node = @head
+    until current_node.nil?
+      if current_node.data == value
+        return true
+        puts "#{value} is within this list!"
+      else
+        current_node = current_node.next
+      end
+    end
   end
 
   def find(value) #returns the index of the node containing value, or nil if not found.
-
+    current_node = @head
+    count = 1
+    until current_node.nil?
+      if current_node.data == value
+        return true
+        puts "#{value} is at node ##{count}!"
+      elsif current_node == @tail && current_node.data != value
+        return nil
+      else
+        count += 1
+        current_node = current_node.next
+      end
+    end
   end
 
   def to_s #represent linked list as strings
